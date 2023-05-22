dfbeec7ea247c53a6c6d2a61489fd7e717ebe901
MARCOS AURÉLIO SOUSA DE CARVALHO <MCARVALHO472@locmed.for.ce>
2023-05-22 13:51:34 -0300

    Commit inicial da rota de produtos

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/primeira-rota', (request, response) => {
-    return response.json({
-        message: 'Acessou a primeira rota com nodemon'
-    })
-})
+const products = [];
+
+app.post('/products')
 
 app.listen(4002, () => console.log('Servidor está rodando na porta 4002'));
