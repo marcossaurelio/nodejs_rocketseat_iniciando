2c16c98c552f0d1d9df290b4b8cff82daf7d1d55
Marcos Aurélio <aurelio257@outlook.com>
2023-05-22 21:07:59 -0300

    Rotas de POST e GET adicionadas e requisições de teste configuradas

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const { randomUUID } = require('crypto');
 
 const app = express();
 
+app.use(express.json());
+
 const products = [];
 
-app.post('/products')
+app.post('/products', (request, response) => {
+    // Nome e preço
+    
+    const { name, price } = request.body;
+
+    const product = {
+        name,
+        price,
+        id: randomUUID()
+    }
+
+    products.push(product);
+
+    return response.json(product);
+});
+
+app.get('/products', (request, response) => {
+    return response.json(products);
+})
+
+app.get('/products/:id', (request, response) =>{
+    const { id } = request.params;
+    const product = products.find(product => product.id === id);
+    return response.json(product);
+})
 
 app.listen(4002, () => console.log('Servidor está rodando na porta 4002'));
